bc648d6c593c505d78d0780a7cf7e9debc3fcd26
arpitraj-dev <arpit.s.nayak@gmail.com>
2025-05-15 08:14:57 +0530

    Your commit message describing the update

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require('express'); 
-const app = express(); 
-const PORT = 3000; 
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => { 
-res.send('Hello, World!'); 
-}); 
-app.listen(PORT, () => { 
-console.log(`Server is running on http://localhost:${PORT}`); 
-}); 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 // npm install
 // dockrer build - t myapp: 1.0 .
-// docker run -p 3000:3000 myapp:1.0 
+// docker run -p 3000:3000 myapp:1.0
+
+// sudo apt install openssh-server
+// su -
+// apt install openssh-server
+// apt install net-tools
+// ifconfig
+// ls
